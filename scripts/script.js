1909d0f1667da0d2526634d743a339c7d020bcdf
feephilips <118175206+feephilips@users.noreply.github.com>
2024-01-21 15:50:09 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -6,15 +6,17 @@ console.log("hi");
 /* menu openen de MENU button */
 /******************************/
 
-/* JOUW CODE HIER - stap 4 */
 
-// stap 1: zoek de menu-button op en sla die op in een variabele
+// stap 1: Hier maak je een variabele aan door de menu-button te zoeken 
+//Opzoeken
 var openButton = document.querySelector("header div> button");
 
-// stap 2: laat de menu-button luisteren naar kliks en voer dan een functie uit
+// stap 2: laat de menu-button luisteren naar kliks en dan voert hij een functie uit
+//functie
 openButton.onclick = openMenu;
 
 // stap 3: voeg in de functie een class toe aan de nav
+//functie maken: open menu
 function openMenu() {  
   // zoek de nav op
   var deNav = document.querySelector("nav");
@@ -28,8 +30,6 @@ function openMenu() {
 /************************************/
 /* menu sluiten met de sluit button */
 /************************************/
-
-/* JOUW CODE HIER - stap 5 */
 
 // stap 1 - zoek sluiten button op
 var sluitButton = document.querySelector("nav button");
@@ -46,14 +46,39 @@ function sluitMenu() {
 
 
 
-/**********************************/
-/* bonus: menu sluiten met escape */
-/**********************************/
-window.onkeydown = handleKeydown;
 
-function handleKeydown(event) {
-  if (event.key == "Escape") {
-    var deNav = document.querySelector("nav");
-    deNav.classList.remove("toonMenu");
-  }
+
+/************************/
+/* CODE VOOR DE HARTJES */
+/************************/
+
+/* alle button met class 'like' in de HTML opzoeken */
+/* omdat er meer buttons geselecteerd moeten worden, querySelectorAll (en niet querySelector) */
+/* het gevonden lijstje met buttons (een array) in de variabele 'heartButtons' opslaan */
+var heartButtons = document.querySelectorAll("main> ul:nth-of-type(5) li button:nth-of-type(2)");
+
+/* elke button in de array laten luisteren naar een klik */
+/* na een klik de functie 'addToFavorites' uitvoeren */
+// heartButtons[0].onclick = addToFavorites;
+// heartButtons[1].onclick = addToFavorites;
+// heartButtons[2].onclick = addToFavorites;
+// heartButtons[3].onclick = addToFavorites;
+/* enzovoort */
+
+/* nb. dit kan met veel minder code met een for loop */
+for (let i = 0; i < heartButtons.length; i++) {
+  heartButtons[i].onclick = addToFavorites;
+}
+
+/* liefde declareren of liefde wegnemen */
+function addToFavorites(event) {
+  /* het hartje waarop geklikt is in de variabele 'clickedHeart' opslaan */
+  let clickedHeart = event.target;
+  
+  /* de parent - de hele fish opzoeken */
+  let theFish = clickedHeart.closest("ul:nth-of-type(5) li");
+  
+  /* de class liked aan de fish toevoegen */
+  /* en weer verwijderen als er nog een keer geklikt wordt */
+  theFish.classList.toggle("liked");
 }
